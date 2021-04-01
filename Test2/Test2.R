8f2116139b301a61bc1e86c2aba5608bae145f1b
kminuse <konstantin.escher@onefootball.com>
2021-04-01 13:44:29 +0000

    commit

---
+++ Test2/Test2.R
@@ -0,0 +1,36 @@
+## Test 2 - 1. April 2021
+
+# Tutorial from this YouTube video: https://www.youtube.com/watch?v=tfN10IUX9Lo
+
+# Shiny Theme Selector: https://shiny.rstudio.com/gallery/shiny-theme-selector.html
+
+# load packaged
+library(shiny)
+library(shinythemes)
+
+
+#Define UI
+ui <- fluidPage(theme = shinytheme("cerulean"),
+  navbarPage(
+    "My first app",
+    tabPanel("Navbar 1",
+             sidebarPanel(
+              tags$h3("Input:"),
+              textInput("txt1", "Vorname:", ""),
+              textInput("txt2", "Nachname:", ""),
+             
+             ), #sidebarPanel
+            mainPanel(
+              h1("Header 1"),
+              h4("Output 1"),
+              verbatimTextOutput("txtOut"),
+            ) # mainPanel
+
+  ), #Navbar 1, tabPanel
+  tabPanel("Navbar 2", "This panel is intentionally left blank"),
+  tabPanel("Navbar 3", "This panel is intentionally left blank")
+  
+  ) #navbarPage
+) #fluidPage
+                
+          
