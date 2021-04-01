f6475ff319b841523d274f025128bf8f553f9529
kminuse <konstantin.escher@onefootball.com>
2021-04-01 14:11:01 +0000

    commit

--- Test2/Test2.R
+++ Test2/Test2.R
@@ -11,26 +11,35 @@ library(shinythemes)
 
 #Define UI
 ui <- fluidPage(theme = shinytheme("cerulean"),
-  navbarPage(
-    "My first app",
-    tabPanel("Navbar 1",
-             sidebarPanel(
-              tags$h3("Input:"),
-              textInput("txt1", "Vorname:", ""),
-              textInput("txt2", "Nachname:", ""),
-             
-             ), #sidebarPanel
-            mainPanel(
-              h1("Header 1"),
-              h4("Output 1"),
-              verbatimTextOutput("txtOut"),
-            ) # mainPanel
-
-  ), #Navbar 1, tabPanel
-  tabPanel("Navbar 2", "This panel is intentionally left blank"),
-  tabPanel("Navbar 3", "This panel is intentionally left blank")
+                navbarPage("My first app",
+                           tabPanel("Navbar 1",
+                                    sidebarPanel(
+                                      tags$h3("Input:"),
+                                      textInput("txt1", "Vorname:", ""),
+                                      textInput("txt2", "Nachname:", ""),
+                                      ), #sidebarPanel
+                                    mainPanel(
+                                      h1("Header 1"),
+                                      h4("Output 1"),
+                                      verbatimTextOutput("txtout"),
+                                      ) # mainPanel
+                                    ), #Navbar 1, tabPanel
+                           tabPanel("Navbar 2", "This panel is intentionally left blank"),
+                           tabPanel("Navbar 3", "This panel is intentionally left blank"),
+                           #tabPanel(h4("About"),
+                            #        includeMarkdown("learning.Rmd")),
   
   ) #navbarPage
 ) #fluidPage
-                
+
+# Define server function
+server <- function(input, output) {
+  output$txtout <- renderText({
+    paste( input$txt1, input$txt2, sep = " ")
+  })
+}
+
+# Create shiny object
+shinyApp(ui = ui, server = server)
+
           
